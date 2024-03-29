e4977a8a08fa72b51266ab05cf9aa3eb9d1aaf7f
anilmaurya61 <anilmaurya0004@gmail.com>
2024-03-29 19:37:21 +0530

    updated

--- defer/main.go
+++ defer/main.go
@@ -7,8 +7,15 @@ func main(){
 	defer fmt.Println("two")
 	defer fmt.Println("three")
 	fmt.Println("four")
+	myDeferFun()
 }
 
 // First in last out
 
 // "one", "two", "three"
+
+func myDeferFun(){
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+}
